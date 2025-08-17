2dccf0356ce23c97ea6e554dae60476ac6e7ea4a
ravisankar404 <ravisankar@campx.in>
2025-08-17 12:36:35 +0530

    user creation added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import express from "express";
-const app = express();
+import { app } from "./app.js";
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
@@ -8,4 +7,12 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
