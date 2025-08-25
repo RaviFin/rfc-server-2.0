5307f4f1dc220493f80fb84256fddb0cdff21646
ravisankar404 <ravisankar@campx.in>
2025-08-25 05:46:19 +0530

    routes added

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -4,6 +4,10 @@ import {
   deleteTransaction,
   getAllTransactions,
   getTransactionById,
+  getTransactionsByAccount,
+  getTransactionsByCustomer,
+  getTransactionsByLoan,
+  getTransactionStats,
   updateTransaction,
 } from "../controllers/transaction.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -12,7 +16,12 @@ const router = Router();
 
 router.post("/", verifyJWT, createTransaction);
 router.get("/", verifyJWT, getAllTransactions);
+router.get("/stats", verifyJWT, getTransactionStats);
 router.get("/:id", verifyJWT, getTransactionById);
+router.get("/customer/:customerId", verifyJWT, getTransactionsByCustomer);
+
+router.get("/loan/:loanId", verifyJWT, getTransactionsByLoan);
+router.get("/account/:accountId", verifyJWT, getTransactionsByAccount);
 router.patch("/:id", verifyJWT, updateTransaction);
 router.delete("/:id", verifyJWT, deleteTransaction);
 
